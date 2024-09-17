18adc721582bb83ca21bb11af6ba255e13af5261
Kammi Henderson <kammi_henry@yahoo.com>
2024-09-17 15:01:25 -0500

    Initial commit

---
+++ src/NotesList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Note from "./Note.js";
+
+const NotesList = (props) => {
+  const keepSearchMatches = (note) => note.doesMatchSearch;
+  const searchMatches = props.notes.filter(keepSearchMatches);
+
+  const renderNote = (note) => (
+    <Note
+      onType={props.onType}
+      note={note}
+      key={note.id}
+      removeNote={props.removeNote}
+    />
+  );
+  const noteElements = searchMatches.map(renderNote);
+  return <ul className="notes-list">{noteElements}</ul>;
+};
+
+export default NotesList;
